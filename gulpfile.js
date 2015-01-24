30c4b8e3bfd2b5ff286140dac801885b6b680611
Paul Salaets <psalaets@gmail.com>
2015-01-23 20:48:33 -0500

    build task that minifies js

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,30 @@ var source = require('vinyl-source-stream');
 var del = require('del');
 var browserSync = require('browser-sync');
 
-gulp.task('watchify', ['clean'], function() {
+var uglify = require('gulp-uglify');
+var rev = require('gulp-rev');
+var inject = require('gulp-inject');
+
+// options.dev = true will...
+//   - activate watchify
+//   - generate inline source maps
+function makeBundle(options) {
+  options = options || {};
+  options.dev = options.dev || false;
+
   var bundler = browserify({
     // begin config settings required by watchify
     cache: {},
     packageCache: {},
-    fullPaths: true
+    fullPaths: true,
     // end config settings required by watchify
+    debug: options.dev // inline source maps
   });
 
-  bundler = watchify(bundler);
-  bundler.on('update', rebundle);
+  if (options.dev) {
+    bundler = watchify(bundler);
+    bundler.on('update', rebundle);
+  }
 
   // entry point into our commonjs modules
   bundler.add('./app/index.js');
@@ -28,6 +41,14 @@ gulp.task('watchify', ['clean'], function() {
   }
 
   return rebundle();
+}
+
+gulp.task('browserify', ['clean'], function() {
+  return makeBundle();
+});
+
+gulp.task('watchify', ['clean'], function() {
+  return makeBundle({dev: true});
 });
 
 gulp.task('watch', ['watchify'], function(cb) {
@@ -50,8 +71,28 @@ gulp.task('watch', ['watchify'], function(cb) {
   });
 });
 
+gulp.task('build-scripts', ['browserify'], function() {
+  return gulp.src('app/build/bundle.js')
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(gulp.dest('build/scripts'));
+});
+
+gulp.task('build-html', ['build-scripts'], function() {
+  var bundleFile = gulp.src('build/scripts/bundle-*.js', {read:false});
+
+  return gulp.src('app/index.html')
+    .pipe(inject(bundleFile, {
+      ignorePath: 'build/',
+      addRootSlash: false
+    }))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', ['build-html']);
+
 gulp.task('clean', function(cb) {
-  del(['app/build'], cb);
+  del(['app/build', 'build'], cb);
 });
 
 gulp.task('default', function() {
@@ -59,6 +100,7 @@ gulp.task('default', function() {
   console.log('Tasks:');
   console.log();
   console.log('  watch - Serve app locally with auto-reload');
+  console.log('  build - Generate deployable app in build/');
   console.log('  clean - Delete generated files');
   console.log();
 });
