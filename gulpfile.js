ba79a66d19c0b57ad3c1501abaa377ebfd7a94c4
Paul Salaets <psalaets@gmail.com>
2015-01-23 20:01:14 -0500

    clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var watchify = require('watchify');
-
 var source = require('vinyl-source-stream');
+var del = require('del');
 
-gulp.task('watchify', function() {
+
+gulp.task('watchify', ['clean'], function() {
   var bundler = browserify({
     // begin config settings required by watchify
     cache: {},
@@ -27,4 +28,8 @@ gulp.task('watchify', function() {
   }
 
   return rebundle();
+});
+
+gulp.task('clean', function(cb) {
+  del(['app/build'], cb);
 });
