e4f4ea53d781e4b38537e37b724fb1ec9933b8c4
Paul Salaets <psalaets@gmail.com>
2015-01-25 01:15:00 -0500

    break dice tumble thing out

--- app/lib/bind-roll-button.js
+++ app/lib/bind-roll-button.js
@@ -1,28 +1,4 @@
-var counter = require('./counter');
-var roll = require('./dice');
-
-var rollResult = document.getElementById('roll-result');
+var diceTumble = require('./dice-tumble');
 
 var rollButton = document.getElementById('roll-button');
 rollButton.addEventListener('click', diceTumble);
-
-function diceTumble() {
-  tumble(roll(), 1);
-  tumble(roll(), 50);
-  tumble(roll(), 100);
-  tumble(roll(), 150);
-  endUpOn(roll(), 200);
-}
-
-function endUpOn(result, delay) {
-  setTimeout(function() {
-    rollResult.textContent = result.roll;
-    counter.increment(result.roll);
-  }, delay);
-}
-
-function tumble(result, delay) {
-  setTimeout(function() {
-    rollResult.textContent = result.roll;
-  }, delay);
-}
---
+++ app/lib/dice-tumble.js
@@ -0,0 +1,27 @@
+var counter = require('./counter');
+var roll = require('./dice');
+
+module.exports = diceTumble;
+
+var rollResult = document.getElementById('roll-result');
+
+function diceTumble() {
+  tumble(roll(), 1);
+  tumble(roll(), 50);
+  tumble(roll(), 100);
+  tumble(roll(), 150);
+  endUpOn(roll(), 200);
+}
+
+function endUpOn(result, delay) {
+  setTimeout(function() {
+    rollResult.textContent = result.roll;
+    counter.increment(result.roll);
+  }, delay);
+}
+
+function tumble(result, delay) {
+  setTimeout(function() {
+    rollResult.textContent = result.roll;
+  }, delay);
+}
