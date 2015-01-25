39ea869bf5fe6c357063f078f9070bdd4ee5c950
Paul Salaets <psalaets@gmail.com>
2015-01-24 19:52:50 -0500

    somehow show what button is pressed

--- app/lib/bind-bottom-toolbar-buttons.js
+++ app/lib/bind-bottom-toolbar-buttons.js
@@ -6,7 +6,18 @@ forEach.call(buttons, function(button) {
     var showId = button.dataset.show;
     var hideId = button.dataset.hide;
 
+    // show chits or dice
     document.getElementById(showId).classList.remove('hidden');
     document.getElementById(hideId).classList.add('hidden');
+
+    // make button look pressed
+    unpressAll(buttons);
+    button.classList.add('pressed');
   });
 });
+
+function unpressAll(buttons) {
+  forEach.call(buttons, function(button) {
+    button.classList.remove('pressed');
+  });
+}
