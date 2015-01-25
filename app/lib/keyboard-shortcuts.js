1643ecea11740a55ee84ced6a3f1de1852826031
Paul Salaets <psalaets@gmail.com>
2015-01-25 12:04:32 -0500

    fix keyboard shortcuts

--- app/lib/keyboard-shortcuts.js
+++ app/lib/keyboard-shortcuts.js
@@ -1,30 +1,38 @@
 var counter = require('./counter');
 var diceTumble = require('./dice-tumble');
 
-// KeyboardEvent#key -> roll
-var rollsByKey = {
-  '2': 2,
-  '3': 3,
-  '4': 4,
-  '5': 5,
-  '6': 6,
-  '7': 7,
-  '8': 8,
-  '9': 9,
-  '0': 10,
-  '-': 11,
-  '=': 12
+// KeyboardEvent#keyCode -> roll
+var rollsByKeyCode = {
+  '50': 2,   // 2 key
+  '51': 3,   // ...
+  '52': 4,
+  '53': 5,
+  '54': 6,
+  '55': 7,
+  '56': 8,
+  '57': 9,   // 9 key
+  '48': 10,  // 0 key
+  '173': 11, // minus in FF
+  '61': 12,  // equals in FF
+  '189': 11, // minus in chrome/safari
+  '187': 12  // equals in chrome/safari
 };
 
 window.addEventListener('keydown', function(event) {
+  /*
+  console.log('keydown, event.key: ' + event.key);
+  console.log('keydown, event.which: ' + event.which);
+  console.log('keydown, event.keyCode: ' + event.keyCode);
+  */
+
   // equivalent to clicking a chit
-  if (event.key in rollsByKey) {
-    counter.increment(rollsByKey[event.key]);
+  if (event.keyCode in rollsByKeyCode) {
+    counter.increment(rollsByKeyCode[event.keyCode]);
     event.preventDefault();
   }
 
   // equivalent to rolling dice
-  if (event.key == ' ') {
+  if (event.keyCode == 32) { // spacebar
     diceTumble();
   }
 });
