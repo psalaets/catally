c1e30c06f158835e18af430d6123cb05f25f93ca
Paul Salaets <psalaets@gmail.com>
2015-01-25 15:34:14 -0500

    first try to animate toggle

--- app/lib/swap-chits-and-dice.js
+++ app/lib/swap-chits-and-dice.js
@@ -3,6 +3,41 @@ var snabbt = require('snabbt.js');
 module.exports = swap;
 
 function swap(hideThis, showThis) {
-  hideThis.classList.add('hidden');
-  showThis.classList.remove('hidden');
+  // millis
+  var instant = 1;
+  var fast = 200;
+
+  snabbt(showThis, {
+    // rotate showThis one by 1/4 to get it ready
+    rotation: [0, Math.PI / 2, 0],
+    easing: 'ease',
+    duration: instant,
+    callback: function() {
+      // show showThis
+      showThis.classList.remove('hidden');
+
+      // rotate hideThis by 1/4
+      snabbt(hideThis, {
+        rotation: [0, Math.PI / 2, 0],
+        easing: 'ease',
+        duration: fast,
+        callback: function() {
+          // hide hideThis
+          hideThis.classList.add('hidden');
+
+          // rotate showThis back 1/4
+          snabbt(showThis, {
+            rotation: [0, -Math.PI / 2, 0],
+            easing: 'ease',
+            duration: fast
+          });
+        }
+      }).then(hideThis, {
+        // rotate hideThis back 1/4 so it's ready for next time
+        rotation: [0, -Math.PI / 2, 0],
+        easing: 'ease',
+        duration: instant
+      });
+    }
+  });
 }
