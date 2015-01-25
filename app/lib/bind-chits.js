5a50a057f0857ea9ddd62f3d926d20b62f9bf0a6
Paul Salaets <psalaets@gmail.com>
2015-01-25 16:43:13 -0500

    wobble effect on chit when pressed

--- app/lib/bind-chits.js
+++ app/lib/bind-chits.js
@@ -1,3 +1,5 @@
+var snabbt = require('snabbt.js');
+
 var counter = require('./counter');
 var forEach = Array.prototype.forEach;
 
@@ -7,5 +9,22 @@ forEach.call(chits, function(chit) {
   chit.addEventListener('click', function() {
     var number = parseInt(chit.dataset.number, 10);
     counter.increment(number);
+
+    wobble(chit);
   });
 });
+
+function wobble(element) {
+  var amount = Math.PI / 3;
+
+  snabbt(element, {
+    rotation: [0, amount, 0],
+    duration: 50,
+    easing: 'ease'
+  }).then({
+    rotationFrom: [0, amount, 0],
+    easing: 'spring',
+    springConstant: 1,
+    springDeacceleration: 0.9,
+  });
+}
