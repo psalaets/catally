65f7a039d4940e92d2d2a0e25b492f786466f292
Paul Salaets <psalaets@gmail.com>
2015-02-27 00:00:27 -0500

    make the swap effect super basic for now

--- app/lib/effects/swap.js
+++ app/lib/effects/swap.js
@@ -3,39 +3,6 @@ var snabbt = require('snabbt.js');
 module.exports = swap;
 
 function swap(hideThis, showThis) {
-  // millis
-  var instant = 1;
-  var fast = 100;
-
-  snabbt(showThis, {
-    // rotate showThis one by 1/4 to get it ready
-    rotation: [0, Math.PI / 2, 0],
-    duration: instant,
-    callback: function() {
-      // show showThis
-      showThis.classList.remove('hidden');
-
-      // rotate hideThis by 1/4
-      snabbt(hideThis, {
-        rotation: [0, Math.PI / 2, 0],
-        easing: 'ease',
-        duration: fast,
-        callback: function() {
-          // hide hideThis
-          hideThis.classList.add('hidden');
-
-          // rotate showThis back 1/4
-          snabbt(showThis, {
-            rotationFrom: [0, Math.PI / 2, 0],
-            easing: 'ease',
-            duration: fast
-          });
-        }
-      }).then(hideThis, {
-        // rotate hideThis back 1/4 so it's ready for next time
-        rotationFrom: [0, Math.PI / 2, 0],
-        duration: instant
-      });
-    }
-  });
+  showThis.classList.remove('hidden');
+  hideThis.classList.add('hidden');
 }
